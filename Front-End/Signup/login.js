1a5a1d57ced4ea7f3fe49033e582ef0858e219ee
Smruthi <166503379+talesoverfables@users.noreply.github.com>
2024-12-13 19:50:25 +0530

    Update login.js

--- Front-End/Signup/login.js
+++ Front-End/Signup/login.js
@@ -9,14 +9,15 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASCCfQxslTnb2eXTT30ofaanwJHA19K0w",
-  authDomain: "artintel-app.firebaseapp.com",
-  projectId: "artintel-app",
-  storageBucket: "artintel-app.firebasestorage.app",
-  messagingSenderId: "227628984562",
-  appId: "1:227628984562:web:769b40dbbdbb9c756d086b",
-  measurementId: "G-5LGFJ6XSWT",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
